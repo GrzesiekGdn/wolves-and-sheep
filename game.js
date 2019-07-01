9ff4d2cd0fc8497f3d429a35809adb33c2d393db
Grzesiek <grzesiek@gdn.waw.pl>
2019-07-02 00:27:54 +0200

    Add sheep move verification,

--- game.js
+++ game.js
@@ -1,31 +1,41 @@
-function initGame(){
+function initGame() {
     return [
-        ['w','','w','','w','','w',''],
-        ['','','','','','','',''],
-        ['','','','','','','',''],
-        ['','','','','','','',''],
-        ['','','','','','','',''],
-        ['','','','','','','',''],
-        ['','','','','','','',''],
-        ['q','','','s','','','','']
+        ['w', '', 'w', '', 'w', '', 'w', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['q', '', '', 's', '', '', '', '']
     ];
 }
 
-function moveSheep(state, up, right){
+function moveSheep(state, up, right) {
     sheep = foundSheep(state);
-    state[sheep.row][sheep.col] = '';
-    state[sheep.row - up][sheep.col + right] = 's';
+
+    var newPos = { row: sheep.row - up, col: sheep.col + right };
+
+    if (isAvailable(state, newPos)) {
+        state[sheep.row][sheep.col] = '';
+        state[newPos.row][newPos.col] = 's';
+    }
     return state;
 }
 
-function foundSheep(state){
-    for(var row = 0; row < 8; row++){
-        for(var col = 0; col < 8; col++){
+function isAvailable(state, newPos) {
+    return newPos.row >= 0 && newPos.col >= 0 && newPos.row < 8 && newPos.col < 8
+        && state[newPos.row][newPos.col] != 'w';
+}
+
+function foundSheep(state) {
+    for (var row = 0; row < 8; row++) {
+        for (var col = 0; col < 8; col++) {
             var val = state[row][col];
-            if(val === 's'){
-                return {row, col};
+            if (val === 's') {
+                return { row, col };
             }
         }
-    }    
+    }
 }
 
