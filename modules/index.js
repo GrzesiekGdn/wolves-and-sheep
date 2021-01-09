97c7f1ff6dff42f7f30633136bd73fa3a7c885cf
Grzesiek <grzesiek@gdn.waw.pl>
2021-01-09 18:15:24 +0100

    Working.

--- modules/index.js
+++ modules/index.js
@@ -1,28 +1,24 @@
 import Game from './game.js';
 import GameRenderer from './gameRenderer.js';
 
-document.getElementById('leftUp').addEventListener('click', () => move(1, -1));
-document.getElementById('rightUp').addEventListener('click', () => move(1, 1));
+document
+  .getElementById('leftUp')
+  .addEventListener('click', () => game.moveSheep(1, -1));
+document
+  .getElementById('rightUp')
+  .addEventListener('click', () => game.moveSheep(1, 1));
 document
   .getElementById('leftDown')
-  .addEventListener('click', () => move(-1, -1));
+  .addEventListener('click', () => game.moveSheep(-1, -1));
 document
   .getElementById('rightDown')
-  .addEventListener('click', () => move(-1, 1));
+  .addEventListener('click', () => game.moveSheep(-1, 1));
 document
   .getElementById('restartGame')
-  .addEventListener('click', () => restart());
+  .addEventListener('click', () => game.initGame());
 
 const board = document.getElementById('chessboard');
 const statusDiv = document.getElementById('statusDiv');
 const gameRenderer = new GameRenderer(board, statusDiv);
 
 const game = new Game(gameRenderer);
-
-const move = (up, right) => {
-  game.moveSheep(up, right);
-};
-
-const restart = () => {
-  game.initGame();
-};
